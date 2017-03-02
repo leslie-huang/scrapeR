e9dcf6e06361555451a55aa26e5c01e1f04f05ca
leslie-huang <lesliehuang@gmail.com>
2017-03-02 14:45:17 -0500

    Include workaround for API rate limit

--- followers_scrapeR.R
+++ followers_scrapeR.R
@@ -3,7 +3,7 @@
 
 ### Set up the workspace
 rm(list=ls())
-setwd("/Users/lesliehuang/Dropbox/scrapeR/")
+setwd("/Users/lesliehuang/Dropbox/twitter-scrapeR/")
 
 set.seed(1234)
 
@@ -12,6 +12,11 @@ lapply(libraries, require, character.only=TRUE)
 
 # Get these from your API:
 # CONSUMER KEY, CONSUMER SECRET, ACCESS TOKEN, ACCESS SECRET
+
+consumer_key <- "L3hRFSHaBtcLqJBd0vyuTNBLQ"
+consumer_secret <- "0xQD1LhefrpcP83pjVJqteyxhsO7Xsiw9tyGDPxRSoOr1F9joc"
+access_token <- "836246147295371264-4bYPgGiBS1iU0L2vxvg2rSvXICWYG9l"
+access_secret <- "w8v89nJmHeJTFKCvTQYOmm3OqRUatpyt3Aco4OtNCgmWO"
 
 setup_twitter_oauth(consumer_key = consumer_key, consumer_secret = consumer_secret,
                     access_token = access_token, access_secret = access_secret)
@@ -43,14 +48,29 @@ generate_edgelist_from_ego_followers <- function(df, ego_name, n) {
 }
 
 # Get initial network of nodes connected to the APSA twitter account
-ego_network <- generate_edgelist_from_ego_followers(twitter_network_edges, "APSAtweets", 10)
+ego_user <- "APSAtweets"
+
+ego_network <- generate_edgelist_from_ego_followers(ego_network, ego_user, 10)
 
 # Get networks of ego's followers
 followers_network <- data.frame(user = character(), follower_name = character())
 
 for (i in 1:length(ego_network$follower_name)) {
   temp_ego <- as.character(ego_network$follower_name[i])
-  followers_network <- generate_edgelist_from_ego_followers(followers_network, temp_ego, 10)
+  temp_ego_user <- getUser(temp_ego)
+  
+  # Pause if rate limit has been exceeded
+  if (as.numeric(getCurRateLimitInfo("followers")$limit[1]) == 0) {
+    Sys.sleep(15 * 60)
+  }
+    
+  # Check if follower has public account; exclude private users
+  if (temp_ego_user$protected == FALSE) {
+    followers_network <- generate_edgelist_from_ego_followers(followers_network, temp_ego, 10)
+  }
+  
+  else {
+    print(paste("Skipping private user", temp_ego$name, sep = " "))
+  }
+    
 }
-
-# Function to filter dataframe for only users in the original network
